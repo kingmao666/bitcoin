b82f4cde4c5e3fea91b150ae2935041485b3d844
QX <test@gmail.com>
2021-11-06 21:21:11 +0800

    添加区块

--- HelloBitCoin.go
+++ HelloBitCoin.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 )
 
@@ -16,14 +17,52 @@ func NewBlock(data string, preBlockHash []byte) *Block {
 		Hash:         []byte{},
 		Data:         []byte(data),
 	}
+	block.SetHash()
 	return &block
 }
 
-func main() {
-	fmt.Println("hello bitcoin")
+func (block *Block) SetHash() {
+	var data []byte
+	data = append(data, block.PreBlockHash...)
+	data = append(data, block.Data...)
+	hash := sha256.Sum256(data)
+	block.Hash = hash[:]
+}
 
-	block := NewBlock("啊啊啊", []byte{0x0000000000000000})
-	fmt.Printf("prv : %x\n", block.PreBlockHash)
-	fmt.Printf("hash : %x\n", block.Hash)
-	fmt.Printf("data : %s\n", block.Data)
+//创建区块链，使用Block数组模拟
+type BlockChain struct {
+	Blocks []*Block
+}
+
+//创建区块链
+func NewBlockChain() *BlockChain {
+	gen := NewBlock("aaa", []byte{0x0000000000000000})
+	bc := BlockChain{Blocks: []*Block{gen}}
+	return &bc
+}
+
+//添加区块
+func (bc *BlockChain) AddBlock(data string) {
+	//1.拿到上一个区块
+	lastBlock := bc.Blocks[len(bc.Blocks)-1]
+	//2.创建区块
+	newBlock := NewBlock(data, lastBlock.Hash)
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func main() {
+	//fmt.Println("hello bitcoin")
+
+	//block := NewBlock("The Times 2009-1-03 Chancellor on brink of second bailout for banks", []byte{0x0000000000000000})
+
+	bc := NewBlockChain()
+
+	bc.AddBlock("第一个新增block")
+
+	for _, blockt := range bc.Blocks {
+		fmt.Printf("prv : %x\n", blockt.PreBlockHash)
+		fmt.Printf("hash : %x\n", blockt.Hash)
+		fmt.Printf("data : %s\n", blockt.Data)
+	}
+
 }
---
+++ TypeTest.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	s := hypot(1, 5)
+	fmt.Println(s)
+}
+
+func hypot(x, y int) (s int) {
+
+	return x + y
+}

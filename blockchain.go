50d16381de141059f315812aaf830ec7e858c524
QX <test@gmail.com>
2022-02-08 09:51:22 -0500

    使用bolt存储区块

--- blockchain.go
+++ blockchain.go
@@ -1,22 +1,106 @@
 package main
 
-//创建区块链，使用Block数组模拟
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+	"os"
+)
+
+//从bolt中加载区块
 type BlockChain struct {
-	Blocks []*Block
+	db   *bolt.DB
+	tail []byte //最后一个区块的哈希值
 }
+
+const blockChainName = "blackChain.db"
+const blockBucketName = "blockBucket"
+const lastHashKey = "lastHashKey"
 
 //创建区块链
 func NewBlockChain() *BlockChain {
-	gen := NewBlock("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", []byte{0x0000000000000000})
-	bc := BlockChain{Blocks: []*Block{gen}}
-	return &bc
+	//gen := NewBlock("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", []byte{0x0000000000000000})
+	//bc := BlockChain{Blocks: []*Block{gen}}
+	//return &bc
+
+	db, err := bolt.Open(blockChainName, 0600, nil)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//defer db.Close()
+
+	var tail []byte
+
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blockBucketName))
+		if b == nil {
+			fmt.Printf("bucket不存在，准备创建！\n")
+			b, err = tx.CreateBucket([]byte(blockBucketName))
+
+			if err != nil {
+				log.Panic(err)
+			}
+			gen := NewBlock("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", []byte{})
+
+			b.Put([]byte(gen.Hash), gen.Serialize())
+			b.Put([]byte(lastHashKey), gen.Hash)
+			tail = gen.Hash
+		} else {
+			tail = b.Get([]byte(lastHashKey))
+		}
+		return nil
+	})
+
+	return &BlockChain{db, tail}
+
+}
+
+//序列化
+func (block *Block) Serialize() []byte {
+	//将block数据转换成字节流
+	var buffer bytes.Buffer
+
+	encoder := gob.NewEncoder(&buffer)
+
+	err := encoder.Encode(block)
+
+	if err != nil {
+		fmt.Println("encode failed", err)
+		os.Exit(1)
+	}
+
+	return buffer.Bytes()
+}
+
+//反序列化
+func Deserialize(data []byte) *Block {
+	fmt.Printf("解码传入的数据： %x\n", data)
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
 }
 
 //添加区块
 func (bc *BlockChain) AddBlock(data string) {
-	//1.拿到上一个区块
-	lastBlock := bc.Blocks[len(bc.Blocks)-1]
-	//2.创建区块
-	newBlock := NewBlock(data, lastBlock.Hash)
-	bc.Blocks = append(bc.Blocks, newBlock)
+	bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blockBucketName))
+		if b == nil {
+			fmt.Printf("bucket不存在，请检查数据！\n")
+			os.Exit(1)
+		}
+		block := NewBlock(data, bc.tail)
+
+		b.Put([]byte(block.Hash), block.Serialize())
+		b.Put([]byte(lastHashKey), block.Hash)
+		bc.tail = block.Hash
+		return nil
+	})
 }
--- main.go
+++ main.go
@@ -1,24 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
 	//fmt.Println("hello bitcoin")
 
 	//block := NewBlock("The Times 2009-1-03 Chancellor on brink of second bailout for banks", []byte{0x0000000000000000})
 
 	bc := NewBlockChain()
+	defer bc.db.Close()
 
 	bc.AddBlock("俺是第一个block")
 
 	bc.AddBlock("俺是第二个block")
 
-	bc.AddBlock("俺是第三个block")
+	//bc.AddBlock("俺是第三个block")
 
-	for idx, block := range bc.Blocks {
+	/*for idx, block := range bc.Blocks {
 		fmt.Printf("======================== %d ===================\n", idx)
 		fmt.Printf("Version : %d\n", block.Version)
 		fmt.Printf("PreBlockHash : %x\n", block.PreBlockHash)
@@ -35,6 +31,6 @@ func main() {
 
 		pow := NewProofOfWork(block)
 		fmt.Printf("Isvaild: %v\n", pow.IsVaild())
-	}
+	}*/
 
 }
